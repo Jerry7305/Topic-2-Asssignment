e9dd36db3355cc2ef9a73c630d48a802de061612
Jeremiah Mol <363325788@GoTVDSB.ca>
2026-09-15 13:18:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Topic_2_Asssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Jeremiah
+            Console.WriteLine(2026 - 16);
+            Console.WriteLine((14 + 15 + 16) / 3);
+            Console.WriteLine((14.0 + 15.0 + 16.0 + 17.0) / 4);
+        }
+    }
+}
