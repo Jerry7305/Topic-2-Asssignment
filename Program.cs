6cd632603b382c7864ec75dd116fa0c25ef89299
Jeremiah Mol <363325788@GoTVDSB.ca>
2026-09-16 13:09:11 -0400

    Done

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@
             Console.WriteLine(2026 - 16);
             Console.WriteLine((14 + 15 + 16) / 3);
             Console.WriteLine((14.0 + 15.0 + 16.0 + 17.0) / 4);
+            Console.WriteLine(Math.Round(Math.PI * Math.Pow(3, 2),1) + "cm^2");
+            Console.WriteLine("C# knows to add when using the + operator instead of sticking things if both values are numeric data types such as a integer or a floating point");
+            
+
+              
         }
     }
 }
